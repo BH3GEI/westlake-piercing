bc57f3e10aaf02092c3b15d1981ef408715635cd
BH3GEI <scholar.liyao@gmail.com>
2026-07-12 01:22:36 +0900

    构建:W-004 固化本机 dlmalloc 源路径

--- local-build-adapters/art-latest/stubs/dlmalloc.c
+++ local-build-adapters/art-latest/stubs/dlmalloc.c
@@ -1,2 +1,2 @@
 // Redirect dlmalloc.c to the A11 external/dlmalloc/malloc.c
-#include "/home/dspfac/aosp-android-11/external/dlmalloc/malloc.c"
+#include "/Users/yao/westlake-local-build/aosp-android-11/external/dlmalloc/malloc.c"
